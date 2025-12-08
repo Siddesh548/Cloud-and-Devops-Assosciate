a75948afe3998744cc513e677a8f49b62038a6e7
Siddu <siddu1234@gmail.com>
2025-12-08 20:06:49 +0530

    VMs

--- WEEK-03/api/demo.py
+++ WEEK-03/api/demo.py
@@ -1,19 +1,22 @@
-import requests ,json
+# import requests ,json
 
-############ GET ###############
+# ############ GET ###############
 
-# response = requests.get("https://jsonplaceholder.typicode.com/posts")
+# # response = requests.get("https://jsonplaceholder.typicode.com/posts")
+# # data = response.json()
+# # print(data) #--->python objects
+# #print(json.dumps(data,indent=4))  #python objects -->json
+
+
+
+# ######### POST ##########
+
+# url = ("https://jsonplaceholder.typicode.com/posts")
+# datas = {'userId':11,'id':95,'title':'python'}
+# response = requests.post(url,json=datas)
 # data = response.json()
-# print(data) #--->python objects
-#print(json.dumps(data,indent=4))  #python objects -->json
+# print(json.dumps(data,indent=4))
+# print('status',response.status_code)
 
 
 
-######### POST ##########
-
-url = ("https://jsonplaceholder.typicode.com/posts")
-datas = {'userId':11,'id':95,'title':'python'}
-response = requests.post(url,json=datas)
-data = response.json()
-print(json.dumps(data,indent=4))
-print('status',response.status_code)
